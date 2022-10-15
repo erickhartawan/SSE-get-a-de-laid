3a01e2e960088d3f84621e4d9caf94301ce13604
rahul <rahul@student-10-201-31-189.wireless.adelaide.edu.au>
2022-10-15 15:24:37 +1030

    helmet and other security features added

--- app.js
+++ app.js
@@ -1,54 +1,75 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 const { Client } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
- 
+
 var client = new Client({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+    port: process.env.PGPORT,
 });
 client.connect();
 
-
-
+const csrf = require("csurf");
+const csrfProtection = csrf();
 var app = express();
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+        useDefaults: false,
+        directives: {
+            defaultSrc: ["'self'"]
+            // scriptSrc: ["'self'", "'unsafe-inline'"],
+            // styleSrc: ["'self'", "fonts.googleapis.com"],
+            // fontSrc: ["'self'", "fonts.gstatic.com"],
+            // connectSrc: ["'self'", "wss://video.geekwisdom.net"],
+        },
+    })
+);
+app.use(csrf());
+app.use(helmet.dnsPrefetchControl({ allow: false })); 
+app.use(helmet.frameguard({ action: 'deny' }));
+app.disable('x-powered-by');
+const sixtyDaysInSeconds = 5184000 // 60 * 24 * 60 * 60
+app.use(helmet.hsts({maxAge: sixtyDaysInSeconds}));
+app.use(helmet.xssFilter());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.disable("x-powered-by");
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  // res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    // res.render('error');
 });
 
-module.exports = app, client;
+(module.exports = app), client, csrfProtection;
